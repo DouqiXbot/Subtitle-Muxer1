55a08b5dc7710bc59b428621c569e8b48ca57393
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2025-02-08 16:10:02 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,7 +1,6 @@
-
 # (c) mohdsabahat
 
-#Logging
+# Logging
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -9,40 +8,40 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 import os
-import pyrogram
+from pyrogram import Client, filters  # Fixed import
 from chat import Chat
 from config import Config
-logging.getLogger('pyrogram').setLevel(logging.WARNING)
 
+logging.getLogger('pyrogram').setLevel(logging.WARNING)
 
 @Client.on_message(filters.command(['help']))
 async def help_user(bot, update):
     if str(update.from_user.id) in Config.ALLOWED_USERS:
         await bot.send_message(
-            update.chat.id,
-            Chat.HELP_TEXT,
-            parse_mode="HTML",  # Fixed parse mode
+            chat_id=update.chat.id,
+            text=Chat.HELP_TEXT,
+            parse_mode="HTML",
             disable_web_page_preview=True,
             reply_to_message_id=update.id  # Fixed message_id
         )
     else:
         await bot.send_message(
-            update.chat.id,
-            Chat.NO_AUTH_USER,
+            chat_id=update.chat.id,
+            text=Chat.NO_AUTH_USER,
             reply_to_message_id=update.id  # Fixed message_id
         )
 
-@pyrogram.Client.on_message(pyrogram.filters.command(['start']))
+@Client.on_message(filters.command(['start']))
 async def start(bot, update):
     if str(update.from_user.id) not in Config.ALLOWED_USERS:
         return await bot.send_message(
-            update.chat.id,
-            Chat.NO_AUTH_USER,
-            reply_to_message_id=update.id  # FIXED
+            chat_id=update.chat.id,
+            text=Chat.NO_AUTH_USER,
+            reply_to_message_id=update.id  # Fixed message_id
         )
 
     await bot.send_message(
         chat_id=update.chat.id,
         text=Chat.START_TEXT,
-        reply_to_message_id=update.id  # FIXED
+        reply_to_message_id=update.id  # Fixed message_id
     )
