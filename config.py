302047c154d324c63c859440d1fe15e87d9bebdb
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2025-02-12 15:08:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,11 @@ class Config:
     BORDER_COLOR = "&H00000000"  # ARGB format (Black)
     BORDER_WIDTH = 1.5
     # Add other existing configurations below...
+LOGO_PATHS = {
+        "default": "logos/logo1.png",
+        "12345678": "logos/logo1.png",
+        "87654321": "logos/logo1.png"
+    }
 
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     APP_ID = os.environ.get('APP_ID', None)
