256f1352b161199e49119ec0db31161f9840146e
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2025-02-12 15:27:29 +0530

    Update muxer.py

--- plugins/muxer.py
+++ plugins/muxer.py
@@ -75,54 +75,53 @@ async def softmux(client, message):
 
 @Client.on_message(filters.command('hardmux') & check_user & filters.private)
 async def hardmux(client, message):
-    
     chat_id = message.from_user.id
     og_vid_filename = db.get_vid_filename(chat_id)
     og_sub_filename = db.get_sub_filename(chat_id)
-    text = ''
-    if not og_vid_filename :
-        text += 'First send a Video File\n'
-    if not og_sub_filename :
-        text += 'Send a Subtitle File!'
+
+    if not og_vid_filename or not og_sub_filename:
+        missing = []
+        if not og_vid_filename:
+            missing.append("Video File")
+        if not og_sub_filename:
+            missing.append("Subtitle File")
+        await client.send_message(chat_id, f"First send a {', '.join(missing)}!")
+        return
+
+    # Get logo path from LOGO_PATHS dictionary
+    logo_path = Config.LOGO_PATHS.get(str(chat_id), Config.LOGO_PATHS["default"])
     
-    if not (og_sub_filename or og_vid_filename) :
-        return await client.send_message(chat_id, text)
-    
-    text = 'Your File is Being Hard Subbed. This might take a long time!'
+    text = "Your File is Being Hard Subbed. This might take a long time!"
     sent_msg = await client.send_message(chat_id, text)
 
-    hardmux_filename = await hardmux_vid(og_vid_filename, og_sub_filename, sent_msg)
-    
+    hardmux_filename = await hardmux_vid(og_vid_filename, og_sub_filename, sent_msg, client, chat_id, logo_path)
+
     if not hardmux_filename:
         return
-    
+
     final_filename = db.get_filename(chat_id)
-    os.rename(Config.DOWNLOAD_DIR+'/'+hardmux_filename,Config.DOWNLOAD_DIR+'/'+final_filename)
-    
+    os.rename(os.path.join(Config.DOWNLOAD_DIR, hardmux_filename), os.path.join(Config.DOWNLOAD_DIR, final_filename))
+
     start_time = time.time()
     try:
         await client.send_video(
-                chat_id, 
-                progress = progress_bar, 
-                progress_args = (
-                    'Uploading your File!',
-                    sent_msg,
-                    start_time
-                    ), 
-                video = os.path.join(Config.DOWNLOAD_DIR, final_filename),
-                caption = final_filename
-                )
-        text = 'File Successfully Uploaded!\nTotal Time taken : {} seconds'.format(round(time.time()-start_time))
-        await sent_msg.edit(text)
+            chat_id, 
+            progress=progress_bar, 
+            progress_args=("Uploading your File!", sent_msg, start_time), 
+            video=os.path.join(Config.DOWNLOAD_DIR, final_filename),
+            caption=final_filename
+        )
+        await sent_msg.edit(f"File Successfully Uploaded!\nTotal Time taken: {round(time.time() - start_time)} seconds")
     except Exception as e:
         print(e)
-        await client.send_message(chat_id, 'An error occured while uploading the file!\nCheck logs for details of the error!')
-    
-    path = Config.DOWNLOAD_DIR+'/'
-    os.remove(path+og_sub_filename)
-    os.remove(path+og_vid_filename)
-    try :
-        os.remove(path+final_filename)
-    except :
+        await client.send_message(chat_id, "An error occurred while uploading the file!\nCheck logs for details.")
+
+    path = Config.DOWNLOAD_DIR
+    os.remove(os.path.join(path, og_sub_filename))
+    os.remove(os.path.join(path, og_vid_filename))
+    try:
+        os.remove(os.path.join(path, final_filename))
+    except:
         pass
+
     db.erase(chat_id)
