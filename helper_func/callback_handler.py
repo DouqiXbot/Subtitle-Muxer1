8c9a2d5f5188795b3ddd75ca142aef9d44f7a27c
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2025-02-17 12:29:48 +0530

    Update callback_handler.py

--- helper_func/callback_handler.py
+++ helper_func/callback_handler.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from pyrogram import Client
 from pyrogram.types import CallbackQuery
 from helper_func.dbhelper import Database as Db
